e03a4a18da6e92a90963305e069d7813d2670bcd
Amanda Murphy <akmurphak@linux1.cs.uchicago.edu>
2022-09-30 19:02:42 -0500

    first upload of files for HW1

---
+++ hw1.py
@@ -0,0 +1,22 @@
+"""
+CMSC 14100, Autumn 2022
+Homework #1
+
+YOUR NAME GOES HERE
+
+People Consulted:
+   For future assignments, you will be expected to list anyone (other than the
+   course staff that you consulted about this assignment).
+
+Online resources consulted:
+   For furture assignments, you will be expected to list the URL of
+   any online resources that you use to complete this assignment.
+"""
+
+def add2():
+   # Replace None with 2 + 2 in the next line.
+    return None
+
+if __name__ == "__main__":
+    print("2 + 2 is", add2())
+    
